0a52e15568dfe24142c4cd2fdab0df2e9729df1b
Dryg1214 <pakazancev901@gmail.com>
2021-11-15 04:15:49 +0400

    Добавьте файлы проекта.

---
+++ Диспетчер задач (Шарп)/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.фToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.запуститьНовуюЗадачуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.параметрыСистемыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обновитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.завершитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.завершитьДеревоПроцессовToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.путьКФайлуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фToolStripMenuItem,
+            this.параметрыСистемыToolStripMenuItem,
+            this.видToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.ShowItemToolTips = true;
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // фToolStripMenuItem
+            // 
+            this.фToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.запуститьНовуюЗадачуToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.фToolStripMenuItem.Name = "фToolStripMenuItem";
+            this.фToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.фToolStripMenuItem.Text = "Файл";
+            // 
+            // запуститьНовуюЗадачуToolStripMenuItem
+            // 
+            this.запуститьНовуюЗадачуToolStripMenuItem.Name = "запуститьНовуюЗадачуToolStripMenuItem";
+            this.запуститьНовуюЗадачуToolStripMenuItem.Size = new System.Drawing.Size(259, 26);
+            this.запуститьНовуюЗадачуToolStripMenuItem.Text = "Запустить новую задачу";
+            this.запуститьНовуюЗадачуToolStripMenuItem.Click += new System.EventHandler(this.запуститьНовуюЗадачуToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(259, 26);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // параметрыСистемыToolStripMenuItem
+            // 
+            this.параметрыСистемыToolStripMenuItem.CheckOnClick = true;
+            this.параметрыСистемыToolStripMenuItem.Name = "параметрыСистемыToolStripMenuItem";
+            this.параметрыСистемыToolStripMenuItem.Size = new System.Drawing.Size(167, 24);
+            this.параметрыСистемыToolStripMenuItem.Tag = "";
+            this.параметрыСистемыToolStripMenuItem.Text = "Параметры системы";
+            this.параметрыСистемыToolStripMenuItem.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.параметрыСистемыToolStripMenuItem.Click += new System.EventHandler(this.параметрыСистемыToolStripMenuItem_Click);
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.обновитьToolStripMenuItem});
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.видToolStripMenuItem.Text = "Вид";
+            // 
+            // обновитьToolStripMenuItem
+            // 
+            this.обновитьToolStripMenuItem.Name = "обновитьToolStripMenuItem";
+            this.обновитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.обновитьToolStripMenuItem.Text = "Обновить";
+            this.обновитьToolStripMenuItem.Click += new System.EventHandler(this.обновитьToolStripMenuItem_Click);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBox1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            this.toolStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip2_ItemClicked);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(59, 24);
+            this.toolStripLabel1.Text = "Поиск: ";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 27);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.завершитьToolStripMenuItem,
+            this.завершитьДеревоПроцессовToolStripMenuItem,
+            this.путьКФайлуToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(288, 76);
+            // 
+            // завершитьToolStripMenuItem
+            // 
+            this.завершитьToolStripMenuItem.Name = "завершитьToolStripMenuItem";
+            this.завершитьToolStripMenuItem.Size = new System.Drawing.Size(287, 24);
+            this.завершитьToolStripMenuItem.Text = "Снять задачу";
+            this.завершитьToolStripMenuItem.Click += new System.EventHandler(this.завершитьToolStripMenuItem_Click);
+            // 
+            // завершитьДеревоПроцессовToolStripMenuItem
+            // 
+            this.завершитьДеревоПроцессовToolStripMenuItem.Name = "завершитьДеревоПроцессовToolStripMenuItem";
+            this.завершитьДеревоПроцессовToolStripMenuItem.Size = new System.Drawing.Size(287, 24);
+            this.завершитьДеревоПроцессовToolStripMenuItem.Text = "Завершить дерево процессов";
+            this.завершитьДеревоПроцессовToolStripMenuItem.Click += new System.EventHandler(this.завершитьДеревоПроцессовToolStripMenuItem_Click);
+            // 
+            // путьКФайлуToolStripMenuItem
+            // 
+            this.путьКФайлуToolStripMenuItem.Name = "путьКФайлуToolStripMenuItem";
+            this.путьКФайлуToolStripMenuItem.Size = new System.Drawing.Size(287, 24);
+            this.путьКФайлуToolStripMenuItem.Text = "Путь к файлу";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 2;
+            this.columnHeader3.Text = "Память";
+            this.columnHeader3.Width = 200;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя процесса";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 1;
+            this.columnHeader2.Text = "ID";
+            this.columnHeader2.Width = 150;
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Default;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader3,
+            this.columnHeader2,
+            this.columnHeader4});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.HoverSelection = true;
+            this.listView1.Location = new System.Drawing.Point(0, 55);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listView1.Size = new System.Drawing.Size(800, 395);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "ЦП";
+            this.columnHeader4.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem фToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem запуститьНовуюЗадачуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem параметрыСистемыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem обновитьToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem завершитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem завершитьДеревоПроцессовToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem путьКФайлуToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Диспетчер задач (Шарп)/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+using Microsoft.VisualBasic;
+
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processes = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetProcesses()//Заполнение и обновление списка процессов
+        {
+            processes.Clear();//очистка списка
+            processes = Process.GetProcesses().ToList<Process>();
+        }
+
+        private void RefreshProcessesList()
+        {
+            listView1.Items.Clear();
+            double memSize = 0; //память 
+            foreach (Process p in processes)// перебор всех процессов
+            {
+                memSize = 0;
+                PerformanceCounter pc = new PerformanceCounter();
+                pc.CategoryName = "Process";
+                pc.CounterName = "Working Set - Private";
+                pc.InstanceName = p.ProcessName;
+
+                memSize = (double)pc.NextValue() / (1024 * 1024);
+
+                string[] row = new string[] { p.ProcessName.ToString(), Math.Round(memSize, 1).ToString(), p.Id.ToString() };
+
+                listView1.Items.Add(new ListViewItem(row));
+                
+                pc.Close();
+                pc.Dispose();
+            }
+            Text = $"Диспетчер задач     (Запущенно процессов : " + processes.Count.ToString() + " )";
+        }
+
+
+        private void RefreshProcessesList(List<Process> processes, string keyword)
+        {
+            listView1.Items.Clear();
+            double memSize = 0; //память 
+            foreach (Process p in processes)// перебор всех процессов
+            {
+                memSize = 0;
+                PerformanceCounter pc = new PerformanceCounter();
+                pc.CategoryName = "Process";
+                pc.CounterName = "Working Set - Private";
+                pc.InstanceName = p.ProcessName;
+
+                memSize = (double)pc.NextValue() / (1024 * 1024);
+
+                string[] row = new string[] { p.ProcessName.ToString(), Math.Round(memSize, 1).ToString(), p.Id.ToString() };
+
+                listView1.Items.Add(new ListViewItem(row));
+
+                pc.Close();
+                pc.Dispose();
+            }
+            Text = $"Диспетчер задач     (Запущенно процессов '{keyword}': )" + processes.Count.ToString();
+        }
+
+        private void killProcess(Process process)
+        {
+            process.Kill();
+
+            process.WaitForExit();
+        }
+        
+        private void killProcessAndChildren(int pid)
+        {
+            if(pid == 0)
+            {
+                return;
+            }
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                "Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection objectCollection = searcher.Get();
+
+            foreach (ManagementObject obj in objectCollection)
+            {
+                killProcessAndChildren(Convert.ToInt32(obj["ProcessID"]));
+            }
+
+            try
+            {
+                Process p = Process.GetProcessById(pid);
+                p.Kill();
+                p.WaitForExit();
+            }
+            catch(ArgumentException) { }
+        }
+
+        private int GetParentProcessId(Process p)
+        {
+            int parentID = 0;
+            try
+            {
+                ManagementObject managementObject = new ManagementObject("win32_process.handle='" + p.Id + "'");
+                managementObject.Get();
+                parentID = Convert.ToInt32(managementObject["ParentProcessID"]);
+            }
+            catch (Exception) { }
+            return parentID;
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            processes = new List<Process>();
+            GetProcesses();
+            RefreshProcessesList();
+        }
+
+        private void обновитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetProcesses();
+            RefreshProcessesList();
+        }
+
+        private void завершитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processTokill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    killProcess(processTokill);
+                    GetProcesses();
+                    RefreshProcessesList();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void завершитьДеревоПроцессовToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processTokill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    killProcessAndChildren(GetParentProcessId(processTokill));
+                    GetProcesses();
+                    RefreshProcessesList();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void запуститьНовуюЗадачуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = Interaction.InputBox("Введите имя программы", "Запуск новой задачи");
+            try
+            {
+                Process.Start(path);
+            }
+            catch (Exception) { }
+        }
+
+        //private void toolStripTextBox2_Click(object sender, EventArgs e)
+        //{
+        //    //string bit_depth = " ";//строка не может быть пустой
+        //    //if (Environment.Is64BitOperatingSystem) 
+        //    //    bit_depth.Replace(" ","Разрядность : 64Bit");
+        //    //else
+        //    //    bit_depth.Replace(" ", "Разрядность : 32Bit");
+        //    ////else Console.WriteLine("Разрядность : 32Bit");
+        //    //string[]parameters = new string[] { "Версия Windows: {0}\n ", Environment.OSVersion.ToString(), 
+        //    //                                    bit_depth, "\n"};
+        //    //параметрыСистемыToolStripMenuItem.Text = "gg не будет";
+        //    // toolStripTextBox2.Text = "gg не будет";//parameters.ToString();
+
+        //    //Environment.OSVersion";
+        //    //Console.WriteLine("Версия Windows: {0}", Environment.OSVersion);
+        //    //if (Environment.Is64BitOperatingSystem) Console.WriteLine("Разрядность : 64Bit");
+        //    //else Console.WriteLine("Разрядность : 32Bit");
+        //    ////Console.WriteLine("Имя компьютера : {0}",Environment.MachineName);
+        //    //Console.WriteLine("Число процессоров : {0}", Environment.ProcessorCount);
+        //    //Console.WriteLine("Системная папка : {0}", Environment.SystemDirectory);
+        //    //Console.WriteLine("Логические диски : {0}", String.Join(", ", Environment.GetLogicalDrives()).Replace(":\\", String.Empty));
+        //    ////Console.WriteLine("WorkingSet: {0}", Environment.WorkingSet / 1024);
+        //    //Console.ReadKey();
+        //}
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        int counter = 0;
+        private void параметрыСистемыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                string bit_depth = " ";
+                if (Environment.Is64BitOperatingSystem)
+                    bit_depth += "Разрядность : 64Bit\n";
+                else
+                    bit_depth += "Разрядность : 32Bit\n";
+                string param = Environment.OSVersion.ToString() + "\n"
+                                + bit_depth + "Имя компьютера : " + Environment.MachineName.ToString() + "\n";
+                ToolStripLabel tsl = new ToolStripLabel(param);
+                //tsl.AutoSize = false;
+                //tsl.AutoToolTip = false;
+                //tsl.Width = 200;
+                tsl.Size = new Size(200, 250);
+                параметрыСистемыToolStripMenuItem.DropDownItems.Add(tsl);
+                //параметрыСистемыToolStripMenuItem.AutoSize = true;
+                //параметрыСистемыToolStripMenuItem.AutoToolTip = true;
+                counter++;
+            }
+        }
+
+        private void toolStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
